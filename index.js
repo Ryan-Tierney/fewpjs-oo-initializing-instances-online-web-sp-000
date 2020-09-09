546be159ef7ab96c32e8f24e80d8b283840c969d
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-09 00:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink; 
+  }
+} 
+
+class Lunch {
+  constructor(salad, soup) { 
+    this.salad = salad; 
+    this.soup = soup;
+  } 
+} 
+
+class Dinner {
+  constuctor(salad, soup, entree, and dessert) { 
+    this.salad = salad; 
+    this.soup = soup; 
+    this.entree = entree; 
+    this._dessert = dessert; 
+} 
